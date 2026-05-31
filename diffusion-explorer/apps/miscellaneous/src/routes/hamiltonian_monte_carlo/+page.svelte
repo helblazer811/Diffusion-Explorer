ab915beb81212aaca6fc53df7b93f15af5d39c33
Alec Helbling <alechelbling@Alecs-MacBook-Pro.local>
2026-05-30 21:28:18 -0400

    HMC route: black page background

--- diffusion-explorer/apps/miscellaneous/src/routes/hamiltonian_monte_carlo/+page.svelte
+++ diffusion-explorer/apps/miscellaneous/src/routes/hamiltonian_monte_carlo/+page.svelte
@@ -2,4 +2,10 @@
   import HamiltonianMonteCarlo from './HamiltonianMonteCarlo.svelte';
 </script>
 
+<svelte:head>
+  <style>
+    html, body { background: #000; color: #eee; }
+  </style>
+</svelte:head>
+
 <HamiltonianMonteCarlo />
